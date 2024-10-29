78dd633718eddada779d0221aeffc897dbe4ad14
runoneall <134772645+runoneall@users.noreply.github.com>
2024-10-29 22:54:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ async function handleRequest(request) {
   const url = `https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@latest/sentences/${randomLetter}.json`;
   const response = await fetch(url);
   const data = await response.json();
-  const randomData = data[Math.floor(Math.random * data.length)];
+  const randomData = data[Math.floor(Math.random() * data.length)];
   const jsonResponse = JSON.stringify(randomData);
   const headers = { 'Content-Type': 'application/json' };
   return new Response(jsonResponse, { headers });
